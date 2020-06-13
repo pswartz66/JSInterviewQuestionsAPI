f47a9083e4f31023977299aff40feeaeb3afc9fb
Phil Swartz <pswartz.sju@gmail.com>
2020-06-13 11:38:44 -0400

    updated submit page

--- client/src/components/Navigation/Navbar/index.js
+++ client/src/components/Navigation/Navbar/index.js
@@ -3,24 +3,28 @@ import './style.css';
 import DrawerButton from '../DrawerButton';
 import NavigationLinks from '../NavigationLinks';
 import JSLogo from '../../../assets/img/jslogo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
 
-    return (
-        <header className="Navbar">
-            <nav className="Navigation">
-                <div className="navButton">
-                    <DrawerButton click={props.drawerClickHandler} />
-                </div>
-                <img src={JSLogo} alt='jslogo' />
-                <div className="navTitle">
-                    InterviewAPI
-                </div>
-                <NavigationLinks />
-            </nav>
+  return (
+    <header className="Navbar">
+      <nav className="Navigation">
+        <div className="navButton">
+          <DrawerButton click={props.drawerClickHandler} />
+        </div>
 
-        </header>
-    )
+        <img src={JSLogo} alt='jslogo' />
+        <div className="navTitle">
+          <Link to="/">
+            InterviewAPI
+          </Link>
+        </div>
+        <NavigationLinks />
+      </nav>
+
+    </header>
+  )
 }
 
 export default Navbar;
--- client/src/components/Submit/index.js
+++ client/src/components/Submit/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import './style.css';
+import Button from 'react-bootstrap/Button';
 
 const Submit = () => {
-    return (
-        <div>
-            Submit page
+  return (
+    <div className="submitContainer">
+      <div className="submitContent">
+        <h2>Question</h2>
+        <div className="submitRow">
+          <textarea className="questionTextArea" placeholder="Enter a question to be added to the API...">
+
+          </textarea>
+
+          <div className="checkboxes">
+            <div className="difficulty">
+              <input type="checkbox" id="easy" name="easyQ" />
+              <label for="Easy">Easy</label>
+            </div>
+            <div className="difficulty">
+              <input type="checkbox" id="medium" name="mmediumQ" />
+              <label for="Medium">Medium</label>
+            </div>
+            <div className="difficulty">
+              <input type="checkbox" id="hard" name="hardQ" />
+              <label for="Hard">Hard</label>
+            </div>
+          </div>
         </div>
-    )
+        <Button
+          className="submitBtn"
+          variant="primary"
+          size="lg">
+          Submit
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default Submit;
