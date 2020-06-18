0229e4beb336fddef23bd3d9a09404ed557abbef
Phil Swartz <pswartz.sju@gmail.com>
2020-06-17 23:45:04 -0400

    added some sample questions

--- client/src/components/Endpoints/index.js
+++ client/src/components/Endpoints/index.js
@@ -2,10 +2,33 @@ import React from 'react';
 import './style.css';
 
 class Endpoints extends React.Component {
-    
+
+
+
 
     render() {
-        
+
+        let all = `
+        {
+          "questions": {
+            "easy": [
+              "What is the difference between let and const?",
+              "What is NAN?",
+              "What is the difference between == and ===?"
+            ],
+            "medium": [
+              "What is a "closure" in Javascript?",
+              "The "this" keyword in Javascript pertains 
+              to what exactly?"  
+            ],
+            "hard": [
+              "Can you explain the difference between prototypical 
+              and class based inheritance?",
+              "What are two factors that must be present 
+              for any recursive function to work correctly?"
+            ],
+          }
+        }`
         return (
             <div className="endpointsContainer">
                 <div className="endpointsContent">
@@ -16,6 +39,12 @@ class Endpoints extends React.Component {
                         <ul className="epItem">All Questions:</ul>
                         <div>https://jsinterview/api/all</div>
 
+                        <pre className="epItemCode">
+
+                            {all}
+
+
+                        </pre>
                         <br></br>
 
                         <ul className="epItem">Easy</ul>
